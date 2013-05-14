172a0701de3e9e1f68c12cbc32c77928c98ec402
Chris Mihalcik <chris.mihalcik@gmail.com>
2013-05-13 21:03:49 -0500

    Refactored calculations out of statement method.

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -1,7 +1,9 @@
 package bad.robot.refactoring.chapter1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Customer {
 
@@ -21,26 +23,70 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
 
-        int frequentRenterPoints = 0;
-
+        // Display header
         String result = "Rental record for " + getName() + "\n";
 
-        for (Rental rental : rentals) {
-
-            double amount = rental.calculateAmout();
-            frequentRenterPoints = rental.calculateFrequentRenterPoints();
-
-            result += "\t" + rental.getTitle() + "\t" + String.valueOf(amount) + "\n";
-
-            totalAmount += amount;
+        // Calc and display line items
+        Map<String, List<Number>> lineItems = calculateStatementLineItems();
+        for (Map.Entry<String, List<Number>> entry : lineItems.entrySet()) {
+            result += "\t" + entry.getKey() + "\t" + String.valueOf(entry.getValue().get(0)) + "\n";
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        // Calc and display summary
+        List<Number> aggegates = calculateStatementAggregates(lineItems);
+        result += "Amount owed is " + String.valueOf(aggegates.get(0)) + "\n";
+        result += "You earned " + String.valueOf(aggegates.get(1)) + " frequent renter points";
 
         return result;
     }
+
+    /**
+     * Calculate rental cost and frequent renter points.
+     *
+     * Might create a LineItem class to encapsulate this data
+     *
+     * @return Returns a map of Rentable.title to a List containing the rental cost followed
+     * by frequent rental ponts.
+     */
+    public Map<String, List<Number>> calculateStatementLineItems() {
+        Map<String, List<Number>> lineItems = new HashMap<String, List<Number>>();
+
+        for (Rental rental: rentals) {
+
+            List<Number> costAndPoints = new ArrayList<Number>();
+            costAndPoints.add(rental.calculateAmout());
+            costAndPoints.add(rental.calculateFrequentRenterPoints());
+
+            lineItems.put(rental.getTitle(), costAndPoints);
+        }
+
+        return lineItems;
+    }
+
+    /**
+     * Calculate the totals for rental cost and frequent renter points.
+     *
+     * Again, a StatementSummery class might hold these values.
+     *
+     * @return a duple, total cost followed by total renter points
+     */
+    public List<Number> calculateStatementAggregates(Map<String, List<Number>> lineItems) {
+
+        ArrayList<Number> aggregates = new ArrayList<Number>();
+        double cost = 0;
+        int points = 0;
+
+        for(List<Number> values : lineItems.values()) {
+            cost += (Double)values.get(0);
+            points += (Integer)values.get(1);
+        }
+
+        aggregates.add(cost);
+        aggregates.add(points);
+
+        return aggregates;
+    }
+
 
 }
